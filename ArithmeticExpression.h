f7cba55a9d997d2b9abbf5b6f263ff154d5f07f4
Kevin Mano <kevin_rotijohn@yahoo.com>
2025-08-20 22:46:43 -0400

    20-August: Added skeleton of Arithmetic Expression header file.

--- ArithmeticExpression.h
+++ ArithmeticExpression.h
@@ -8,8 +8,20 @@ Brief:
 #include <stdio.h>
 #include <math.h>
 #include <string>
+#include "Expression.h"
 
-class ArithmeticExpression{
+class ArithmeticExpression : public Expression{
+    Expression left;
+    Expression right;
 
+    //evaluate the left expression and right expression.
+    std::string evaluate(){
+        
+        void print(); //prints expression
+    }
 
-}
+    float convert(std::string s){
+        //converts a string (as would be returned by evaluate) to a float.
+    }
+
+};
