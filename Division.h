6d37967d65fc2860add25403e58f8e5fccc68968
Kevin Mano <kevin_rotijohn@yahoo.com>
2025-08-20 22:55:11 -0400

    20-August: Added all arithmetic operation header files.

---
+++ Division.h
@@ -0,0 +1,18 @@
+/*
+Author: Kevin Mano
+Brief: 
+
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <string>
+#include "ArithmeticExpression.h"
+
+class Divide: public ArithmeticExpression{
+    std::string evaluate(){}
+
+    //(left '/' right)
+    void print(){}
+};
